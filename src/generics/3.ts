24a9661cb7569b576c4897c6063cd74cd6921bb0
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2024-05-28 08:53:07 +0300

    fixed generics #3

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -2,6 +2,6 @@
 //   return Object.assign(objA, objB);
 // }
 
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return { ...objA, ...objB };
 }
